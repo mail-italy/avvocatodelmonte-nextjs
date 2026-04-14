68eb7566667a272b975098f952bf3fd9b404b179
mail-italy <mail@avvocatodelmonte.com>
2026-04-15 01:03:35 +0200

    Aggiornamento sito

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 
 import './globals.css';
+import Script from 'next/script';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 import FloatingContact from '@/components/FloatingContact';
@@ -27,7 +28,24 @@ export const metadata = buildMetadata({
 export default function RootLayout({ children }) {
   return (
     <html lang="it">
+      <head>
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-PR5RTFTX');`}
+        </Script>
+      </head>
       <body>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-PR5RTFTX"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
         <a className="skip-link" href="#contenuto">Salta al contenuto</a>
         <JsonLd data={[websiteSchema(), attorneySchema(), legalServiceSchema(), localBusinessSchema()]} />
         <Header />
